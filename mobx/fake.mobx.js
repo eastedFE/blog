200a7e6dbd7f4c6c6484a1e8aeb1d9db6f703683
yangtao <yangtao@easted.com.cn>
2018-05-02 21:58:31 +0800

    add history

---
+++ mobx/fake.mobx.js
@@ -0,0 +1,15 @@
+/**
+	extendObservable
+	构造函数添加observable功能
+	@param target
+	@param properties
+	@param decorators
+	@param options
+ **/
+function extendObservable(target, properties, decorators, options){
+
+	
+
+	
+}
+
